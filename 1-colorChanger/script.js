ed5fc87aa090a52f785c2d3dc3e2118a1c6103ab
Nitish sharma <sharmanitish183@gmail.com>
2024-02-07 11:26:04 +0530

    project1 completed

---
+++ 1-colorChanger/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (button){
+        button.addEventListener('click', function (e){
+            console.log(e);
+            if(e.target.id === 'grey'){
+                body.style.backgroundColor = e.target.id;
+            }
+            if(e.target.id === 'white'){
+                body.style.backgroundColor = e.target.id;
+            }
+            if(e.target.id === 'blue'){
+                body.style.backgroundColor = e.target.id;
+            }
+            if(e.target.id === 'yellow'){
+                body.style.backgroundColor = e.target.id;
+            }
+            if(e.target.id === 'purple'){
+                body.style.backgroundColor = e.target.id;
+            }
+        })
+})
